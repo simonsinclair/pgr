0b508872b892016c044f1034f79e78c7ba03dd01
Simon Sinclair <simon@simonsinclair.com>
2021-05-22 18:38:22 +0100

    Add initial pgr and getPageRange function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-const pgr = (): void => {
-  return;
+type Pgr = (
+  pageCount: number,
+  currentPage: number,
+  displayCount: number,
+) => number[];
+
+export const getPageRange = (count: number, startPage: number): number[] => {
+  return Array.from({ length: count }, (_, increment) => startPage + increment);
+};
+
+const pgr: Pgr = (pageCount, currentPage, displayCount) => {
+  let pages;
+
+  /**
+   * - if, at beginning
+   * - if, at end
+   * - if, in middle
+   */
+  if (currentPage < Math.floor(displayCount / 2) + 1) {
+    pages = getPageRange(displayCount, 1);
+  } else if (currentPage >= pageCount - Math.floor(displayCount / 2)) {
+    pages = getPageRange(displayCount, pageCount - displayCount + 1);
+  } else {
+    const offset = displayCount % 2 ? 0 : 1;
+    pages = getPageRange(
+      displayCount,
+      currentPage - Math.floor(displayCount / 2) + offset,
+    );
+  }
+
+  return pages;
 };
 
 export default pgr;
