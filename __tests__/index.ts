4e3a72c783a86bf0fb76534f7d9ec0951be74d87
Simon Sinclair <simon@simonsinclair.com>
2021-05-22 18:38:08 +0100

    Add initial tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,5 +1,35 @@
-import pgr from '../src';
+import pgr, { getPageRange } from '../src';
 
-it('returns undefined', () => {
-  expect(pgr()).toBeUndefined();
+describe('getPageRange', () => {
+  it('returns a page range', () => {
+    expect(getPageRange(10, 1)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(getPageRange(5, 2)).toEqual([2, 3, 4, 5, 6]);
+  });
+});
+
+describe('pgr', () => {
+  it('paginates odd number of page', () => {
+    expect(pgr(9, 1, 7)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    expect(pgr(9, 2, 7)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    expect(pgr(9, 3, 7)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    expect(pgr(9, 4, 7)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    expect(pgr(9, 5, 7)).toEqual([2, 3, 4, 5, 6, 7, 8]);
+    expect(pgr(9, 6, 7)).toEqual([3, 4, 5, 6, 7, 8, 9]);
+    expect(pgr(9, 7, 7)).toEqual([3, 4, 5, 6, 7, 8, 9]);
+    expect(pgr(9, 8, 7)).toEqual([3, 4, 5, 6, 7, 8, 9]);
+    expect(pgr(9, 9, 7)).toEqual([3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it('paginates an even number of pages', () => {
+    expect(pgr(10, 1, 6)).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(pgr(10, 2, 6)).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(pgr(10, 3, 6)).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(pgr(10, 4, 6)).toEqual([2, 3, 4, 5, 6, 7]);
+    expect(pgr(10, 5, 6)).toEqual([3, 4, 5, 6, 7, 8]);
+    expect(pgr(10, 6, 6)).toEqual([4, 5, 6, 7, 8, 9]);
+    expect(pgr(10, 7, 6)).toEqual([5, 6, 7, 8, 9, 10]);
+    expect(pgr(10, 8, 6)).toEqual([5, 6, 7, 8, 9, 10]);
+    expect(pgr(10, 9, 6)).toEqual([5, 6, 7, 8, 9, 10]);
+    expect(pgr(10, 10, 6)).toEqual([5, 6, 7, 8, 9, 10]);
+  });
 });
