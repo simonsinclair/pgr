2a5f40718d22ee1d09060a917209e712e3337415
Simon Sinclair <simon@simonsinclair.com>
2021-06-02 00:46:52 +0100

    Add callback parameter

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,40 @@
+type Callback = (page: number) => unknown;
+
+type GetRange = (
+  length: number,
+  startIndex: number,
+  callback: Callback,
+) => unknown[];
+
 type Pgr = (
   totalPages: number,
   currentPage: number,
   length: number,
-) => number[];
+  callback?: Callback,
+) => unknown[];
 
-export const getRange = (count: number, startIndex: number): number[] => {
-  return Array.from({ length: count }, (_, index) => startIndex + index);
+export const getRange: GetRange = (length, startIndex, callback) => {
+  return Array.from({ length }, (_, index) => callback(startIndex + index));
 };
 
-const pgr: Pgr = (totalPages, currentPage, length) => {
-  if (length >= totalPages) return getRange(totalPages, 1);
-
+const pgr: Pgr = (
+  totalPages,
+  currentPage,
+  rawLength,
+  callback = (page) => page,
+) => {
+  const length = Math.min(totalPages, rawLength);
   const centerPage = Math.floor(length / 2);
-  let pages;
 
+  let pages;
   if (currentPage < centerPage + 1) {
-    pages = getRange(length, 1);
+    pages = getRange(length, 1, callback);
   } else if (currentPage >= totalPages - centerPage) {
-    pages = getRange(length, totalPages - length + 1);
+    pages = getRange(length, totalPages - length + 1, callback);
   } else {
     const offset = length % 2 ? 0 : 1;
-    pages = getRange(length, currentPage - centerPage + offset);
+    pages = getRange(length, currentPage - centerPage + offset, callback);
   }
-
   return pages;
 };
 
